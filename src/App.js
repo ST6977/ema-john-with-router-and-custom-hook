06a645fb609544c0fd42efde115ad5e2271cea6a
ST6977 <sumaiyat610@gmail.com>
2022-08-07 10:20:54 -0700

    all added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import Shop from './components/Shop/Shop'; 
+import OrderReview from './components/OrderReview/OrderReview';
+import{BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import PlaceOrder from './components/PlaceOrder/PlaceOrder';
 
 function App() {
   return (
     <div>
-      <Header></Header>
+           <Header></Header>
+    <Router>
+ 
+    <Switch>
+   <Route exact path ="/">
+  <Shop></Shop>
+   </Route>
+
+   <Route exact path ="/shop">
+  <Shop></Shop>
+   </Route>
+
+   <Route  path ="/review">
+  <OrderReview></OrderReview>
+   </Route>
+
+   <Route  path ="/inventory">
+  <Inventory></Inventory>
+   </Route>
+
+
+   {/* <Route  path ="/placeorder">
+ <PlaceOrder></PlaceOrder>
+   </Route> */}
+
+   <Route  path ="/*">
+  <NotFound></NotFound>
+   </Route>
+    </Switch>
+    </Router>
+     
       <Shop></Shop>
     </div>
   );
---
+++ src/components/PlaceOrder/PlaceOrder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const PlaceOrder = () => {
+    return (
+        <div>
+            <h1>jjjjjj</h1>
+        </div>
+    );
+};
+
+export default PlaceOrder;
